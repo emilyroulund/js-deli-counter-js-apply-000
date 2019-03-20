ae9eb7cec5b1cf13a057e9618120ba0545b2776c
merry-lab-3791 <emilyroulund@gmail.com>
2019-03-20 17:46:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-//purpose: add new person to the end of the currentline 
+//purpose: greet new customer and tell them position in line 
+//logic: add new person to the end of the currentline 
 function  takeANumber(currentLine, newName){
   currentLine.push(newName);
-  return `Welcome, ${newName}. You are number ${currentLine.length} in line.`
+  return `Welcome, ${newName}. You are number ${currentLine.length} in line.`;
 }
 
-/*purpose: accept the current line of people
+
+/*purpose: announces who is being served and remove them from line
+ *logic: accept the current line of people
  *return the first person in line, 
  *then remove that individual from the line */
 function nowServing (currentLine){
@@ -14,14 +17,17 @@ function nowServing (currentLine){
     return `Currently serving ${currentLine.shift()}.`;
 }
 
-//purpose: accept current line of people and returns it as string
+//purpose: announce number and name of every person in line
+/* logic: accept current line of people 
+* create new array that adjusts index to be relatable position in line 
+ * returns string announcing every name and respective number in line*/
 function currentLine (line){
   if (line.length===0){
-    return "The line is currently empty." ;
+    return "The line is currently empty.";
   }
   var newLine = []; 
   for (let i = 0; i< line.length; i++){
     newLine.push(` ${(i+1)}. ${line[i]}`);
   }
-  return `The line is currently:${newLine}`
+  return `The line is currently:${newLine}`;
 }
